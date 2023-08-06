cbcdd070fd6d3c29feef8f5e6e099110b4e1a989
TakWolf <takwolf@foxmail.com>
2023-08-06 22:44:01 +0800

    调整 typing 写法

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,6 +1,6 @@
 from configs.font_config import FontConfig
 
 font_configs = FontConfig.loads()
-outputs_name_to_config: dict[str, FontConfig] = {font_config.outputs_name: font_config for font_config in font_configs}
+outputs_name_to_config = {font_config.outputs_name: font_config for font_config in font_configs}
 
 font_formats = ['otf', 'woff2', 'ttf', 'bdf']
